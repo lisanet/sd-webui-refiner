denoiser_callback(): fix refiner steps calculation and make sure that we never have more than 20% refiner steps.

scripts/refiner.py
@@ -96,7 +96,7 @@ class Refiner(scripts.Script):
  
         
         def denoiser_callback(params: script_callbacks.CFGDenoiserParams):
-            if params.sampling_step > params.total_sampling_steps * 0.8 - 2:
+            if params.sampling_step > params.total_sampling_steps - int(params.total_sampling_steps * 0.2) - 2:
                 params.text_cond['vector'] = torch.cat((params.text_cond['vector'][:, :2304], self.c_ae), 1)
                 params.text_uncond['vector'] = torch.cat((params.text_uncond['vector'][:, :2304], self.uc_ae), 1)
                 params.text_cond['crossattn'] = params.text_cond['crossattn'][:, :, -1280:]